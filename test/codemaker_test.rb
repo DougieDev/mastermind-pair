72ac640394ce41714272635dd37e9194c053e509
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-02-11 17:27:01 -0600

    Test codemaker exists and has attributes

--- test/codemaker_test.rb
+++ test/codemaker_test.rb
@@ -4,5 +4,15 @@ require './lib/codemaker'
 
 class CodeMakerTest < Minitest::Test
   def setup
+    @codemaker = Codemaker.new
+  end
+
+  def test_it_exists
+    assert_instance_of Codemaker, @codemaker
+  end
+
+  def test_it_starts_with_colors_and_empty_code_array
+    assert_equal @codemaker.colors, ["R", "G", "B", "Y"]
+    assert_equal @codemaker.code, []
   end
 end
