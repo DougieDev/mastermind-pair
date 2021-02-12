015409a6a5e963d73ca73c7692df1a25fc99c288
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-02-12 11:24:26 -0600

    Test it exists

--- test/codebreaker_test.rb
+++ test/codebreaker_test.rb
@@ -1,8 +1,15 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/codebreaker'
+require './lib/codemaker'
 
-class CodeBreakerTest < Minitest::Test
+class CodebreakerTest < Minitest::Test
   def setup
+    @codemaker = Codemaker.new
+    @player = Codebreaker.new(@codemaker)
+  end
+
+  def test_it_exists
+    assert_instance_of Codebreaker, @player
   end
 end
