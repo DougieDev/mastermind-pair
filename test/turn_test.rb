2338cc60edee4a5803504b676d472d470d158805
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-02-13 15:57:24 -0600

    Test for guess length

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -9,10 +9,25 @@ class TurnTest < Minitest::Test
   def setup
     @codemaker = Codemaker.new
     @codebreaker = Codebreaker.new(@codemaker)
-    @turn = Turn.new(@codemaker, @codebreaker)
+    @message = Message.new
+    @turn = Turn.new(@codemaker, @codebreaker, @message)
   end
 
   def test_it_exists
     assert_instance_of Turn, @turn
+  end
+
+  def test_it_can_find_code
+    assert_equal @codemaker.code, @turn.find_code
+  end
+
+  def test_it_can_check_length
+    @codebreaker.guess_code("rrr")
+
+    assert_equal @message.short_answer, @turn.guess_length_check
+
+    @codebreaker.guess_code("rrryy")
+
+    assert_equal @message.long_answer, @turn.guess_length_check
   end
 end
