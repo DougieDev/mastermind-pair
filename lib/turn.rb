e698a41811da80ca76dc81347d473ea1d2ec36d2
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-02-13 15:33:38 -0600

    Add initialize method

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,6 +1,13 @@
 class Turn
   attr_reader
-  
-  def initialize
+
+  def initialize(codemaker, codebreaker)
+    @codemaker = codemaker
+    @codebreaker = codebreaker
+    @message = Message.new
+    @guess = nil
+    @winner = false
   end
+
+
 end
