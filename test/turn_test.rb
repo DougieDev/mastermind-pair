55b1f2ef766d4f224fa2138b56c560b3acfcaa69
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-02-13 15:34:03 -0600

    est it exists

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -1,8 +1,18 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/turn'
+require './lib/codemaker'
+require './lib/codebreaker'
+require './lib/message'
 
 class TurnTest < Minitest::Test
   def setup
+    @codemaker = Codemaker.new
+    @codebreaker = Codebreaker.new(@codemaker)
+    @turn = Turn.new(@codemaker, @codebreaker)
+  end
+
+  def test_it_exists
+    assert_instance_of Turn, @turn
   end
 end
