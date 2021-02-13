34df5510363326f6cec5487436f77b891a7cbdf3
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-02-13 14:55:21 -0600

    Create method for code guess

--- lib/codebreaker.rb
+++ lib/codebreaker.rb
@@ -1,7 +1,11 @@
 class Codebreaker
-  attr_reader :codemaker
+  attr_reader :codemaker, :guess
 
   def initialize(codemaker)
     @codemaker = codemaker
+  end
+
+  def guess_code(player_input)
+    @guess = player_input.upcase.split("")
   end
 end
