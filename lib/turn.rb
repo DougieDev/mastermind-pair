03ba96e6d1d78c47072ad881ed2f7af030ec066b
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-02-13 15:57:00 -0600

    Add guess length check method

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,13 +1,27 @@
 class Turn
-  attr_reader
+  attr_reader :message, :codemaker, :codebreaker
 
-  def initialize(codemaker, codebreaker)
+  def initialize(codemaker, codebreaker, message)
     @codemaker = codemaker
     @codebreaker = codebreaker
-    @message = Message.new
+    @message = message
     @guess = nil
     @winner = false
   end
+
+  def find_code
+    @codemaker.code
+  end
+
+  def guess_length_check
+    if @codebreaker.guess.length < 4
+        @message.short_answer
+    elsif @codebreaker.guess.length > 4
+           @message.long_answer
+    end
+  end
+
+
 
 
 end
