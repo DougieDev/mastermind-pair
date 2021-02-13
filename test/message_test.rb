28d9ae57d30d8923d26fa4deab97eabaf9501c61
Carlyle Douglas <cdouglascorp@gmail.com>
2021-02-13 12:55:01 -0700

    Add test for user prompt input

--- test/message_test.rb
+++ test/message_test.rb
@@ -15,7 +15,11 @@ class MessageTest < Minitest::Test
     assert_equal "Welcome to MASTERMIND", @message.welcome_message
   end
 
-  
+  def test_user_input_prompt
+    prompt = "Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
+    assert_equal prompt, @message.user_input_prompt
+  end
+
 
 
 end
