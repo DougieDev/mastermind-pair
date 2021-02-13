742b4bedb9e90ea4a2772eac683f85e0a04dff81
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-02-13 16:20:12 -0600

    Add method to check number of correct positions

--- lib/turn.rb
+++ lib/turn.rb
@@ -29,7 +29,12 @@ class Turn
     @codemaker.code.length - (@codemaker.code - @codebreaker.guess).count
   end
 
-
-
+  def num_correct_positions
+    @zipped_array = @codemaker.code.zip(@codebreaker.guess)
+    @num_correct_positions = @zipped_array.select do |array|
+      array[0] == array[1]
+    end
+    @num_correct_positions.count
+  end
 
 end
