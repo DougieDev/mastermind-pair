fc1a68b92a5274e64e9804bdcbf90299e98632ce
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-02-16 21:50:19 -0600

    Finish end game method

--- lib/game.rb
+++ lib/game.rb
@@ -18,12 +18,13 @@ class Game
 
   def start_game
     @message.welcome_message
-    @message.user_input_prompt
+    # @message.user_input_prompt
     @codemaker.randomize
     start_loop
   end
 
   def start_loop
+    @message.user_input_prompt
     @user_input = gets.chomp.downcase
     initial_selection
   end
@@ -42,6 +43,7 @@ class Game
       game_flow
     elsif @user_input == 'i'
       @message.instructions
+      start_loop
     elsif @user_input == 'q'
       @message.quit
       abort
@@ -52,15 +54,15 @@ class Game
 
 
   def game_flow
-    until @turn.has_won? do 
+    until @turn.has_won? do
       @user_guess_prompt = gets.chomp
-      if @user_guess_prompt == 'q'
-        @message.quit
-        abort
-      elsif @user_guess_prompt == 'c'
-        show_cheat
-        game_flow
-      end
+        if @user_guess_prompt == 'q'
+          @message.quit
+          abort
+        elsif @user_guess_prompt == 'c'
+          show_cheat
+          game_flow
+        end
       @codebreaker.guess_code(@user_guess_prompt)
       @turn.add_guess
       if @codebreaker.guess == @codemaker.code
@@ -80,8 +82,19 @@ class Game
   end
 
   def end_game_prompt
-    p "Congratulations! You guessed the sequence #{@codemaker.code.join} in #{@turn.number_of_guesses} guesses!"
-    p "Do you want to (p)lay again or (q)uit?"
+    puts "Congratulations! You guessed the sequence #{@codemaker.code.join} in #{@turn.number_of_guesses} guesses!"
+    puts "Do you want to (p)lay again or (q)uit?"
+    @response = gets.chomp
+      if @response == 'p'
+        @codemaker.code.clear
+        @codemaker.randomize
+        intro
+        game_flow
+      elsif @response == 'q'
+        @message.quit
+        abort
+      end
+
     # initial_selection
   end
 end
